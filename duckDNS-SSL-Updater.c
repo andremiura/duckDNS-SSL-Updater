24ce6488047cfac05b50fd3ce48148a72e04c37a
Andre Miura <andremiura@gmail.com>
2023-06-19 12:22:15 -0300

    correcaoDeBugRefatIA

--- duckDNS-SSL-Updater.c
+++ duckDNS-SSL-Updater.c
@@ -61,11 +61,10 @@ int main(int argc, char *argv[]){
                 exit(-1);
         }
 
-        if( strlen(subdomain > 40) || strlen(token > 36) ){
+        if( strlen(subdomain) > 40 || strlen(token) > 36 ){
                 printf("\nSize error!\n\n");
                 exit(-1);
         }
-
         fd = socket_connect(hostname,port); 
 
 	SSL_load_error_strings ();
